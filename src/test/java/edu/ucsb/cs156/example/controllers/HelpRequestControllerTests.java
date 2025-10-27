501dc52c039fa128b9adac1ccbf5380fa37b00fd
Hauke Rehwald <hauke_rehwald@ucsb.edu>
2025-10-26 22:00:29 -0700

    Cotroller file for helprequest hauke

--- src/test/java/edu/ucsb/cs156/example/controllers/HelpRequestControllerTests.java
+++ src/test/java/edu/ucsb/cs156/example/controllers/HelpRequestControllerTests.java
@@ -1,16 +1,9 @@
 package edu.ucsb.cs156.example.controllers;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import edu.ucsb.cs156.example.ControllerTestCase;
@@ -19,44 +12,39 @@ import edu.ucsb.cs156.example.repositories.HelpRequestRepository;
 import edu.ucsb.cs156.example.repositories.UserRepository;
 import edu.ucsb.cs156.example.testconfig.TestConfig;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.web.servlet.MvcResult;
 
 @WebMvcTest(controllers = HelpRequestController.class)
 @Import(TestConfig.class)
 public class HelpRequestControllerTests extends ControllerTestCase {
-    @MockBean HelpRequestRepository helpRequestRepository;
+  @MockBean HelpRequestRepository helpRequestRepository;
 
-    @MockBean UserRepository userRepository;
+  @MockBean UserRepository userRepository;
 
-      // Authorization tests for /api/helpRequests/admin/all
+  // Authorization tests for /api/helpRequests/admin/all
 
-    @Test
-    public void logged_out_users_cannot_get_all() throws Exception {
-        mockMvc
-            .perform(get("/api/helprequests/all"))
-            .andExpect(status().is(403)); // logged out users can't get all
-    }
+  @Test
+  public void logged_out_users_cannot_get_all() throws Exception {
+    mockMvc
+        .perform(get("/api/helprequests/all"))
+        .andExpect(status().is(403)); // logged out users can't get all
+  }
 
-    @WithMockUser(roles = {"USER"})
-    @Test
-    public void logged_in_users_can_get_all() throws Exception {
-        mockMvc.perform(get("/api/helprequests/all")).andExpect(status().is(200)); // logged
-    }
+  @WithMockUser(roles = {"USER"})
+  @Test
+  public void logged_in_users_can_get_all() throws Exception {
+    mockMvc.perform(get("/api/helprequests/all")).andExpect(status().is(200)); // logged
+  }
 
-    @WithMockUser(roles = {"USER"})
-    @Test
-    public void user_get_all_returns_data() throws Exception {
-        var h = HelpRequest.builder()
+  @WithMockUser(roles = {"USER"})
+  @Test
+  public void user_get_all_returns_data() throws Exception {
+    var h =
+        HelpRequest.builder()
             .requesterEmail("a@b.com")
             .teamId("T1")
             .tableOrBreakoutRoom("Table1")
@@ -65,40 +53,40 @@ public class HelpRequestControllerTests extends ControllerTestCase {
             .requestTime(LocalDateTime.parse("2025-12-23T10:00:00"))
             .build();
 
-        when(helpRequestRepository.findAll()).thenReturn(List.of(h));
+    when(helpRequestRepository.findAll()).thenReturn(List.of(h));
 
-        mockMvc.perform(get("/api/helprequests/all"))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$", hasSize(1)))
-            .andExpect(jsonPath("$[0].requesterEmail", is("a@b.com")))
-            .andExpect(jsonPath("$[0].teamId", is("T1")));
+    mockMvc
+        .perform(get("/api/helprequests/all"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$", hasSize(1)))
+        .andExpect(jsonPath("$[0].requesterEmail", is("a@b.com")))
+        .andExpect(jsonPath("$[0].teamId", is("T1")));
 
-        // Kills VoidMethodCall mutants
-        verify(helpRequestRepository).findAll();
-    }
+    // Kills VoidMethodCall mutants
+    verify(helpRequestRepository).findAll();
+  }
 
+  // Authorization tests for /api/ucsbdates/post
+  // (Perhaps should also have these for put and delete)
 
-    
-    // Authorization tests for /api/ucsbdates/post
-    // (Perhaps should also have these for put and delete)
+  @Test
+  public void logged_out_users_cannot_post() throws Exception {
+    mockMvc.perform(post("/api/helprequests/post")).andExpect(status().is(403));
+  }
 
-    @Test
-    public void logged_out_users_cannot_post() throws Exception {
-        mockMvc.perform(post("/api/helprequests/post")).andExpect(status().is(403));
-    }
+  @WithMockUser(roles = {"USER"})
+  @Test
+  public void logged_in_regular_users_cannot_post() throws Exception {
+    mockMvc
+        .perform(post("/api/helprequests/post"))
+        .andExpect(status().is(403)); // only admins can post
+  }
 
-    @WithMockUser(roles = {"USER"})
-    @Test
-    public void logged_in_regular_users_cannot_post() throws Exception {
-        mockMvc
-            .perform(post("/api/helprequests/post"))
-            .andExpect(status().is(403)); // only admins can post
-    }
-
-    @WithMockUser(roles = {"USER"})
-    @Test
-    public void user_get_all_returns_data() throws Exception {
-        var h = HelpRequest.builder()
+  @WithMockUser(roles = {"USER"})
+  @Test
+  public void user_get_all_returns_data() throws Exception {
+    var h =
+        HelpRequest.builder()
             .requesterEmail("a@b.com")
             .teamId("T1")
             .tableOrBreakoutRoom("Table1")
@@ -107,17 +95,16 @@ public class HelpRequestControllerTests extends ControllerTestCase {
             .requestTime(LocalDateTime.parse("2025-12-23T10:00:00"))
             .build();
 
-        when(helpRequestRepository.findAll()).thenReturn(List.of(h));
+    when(helpRequestRepository.findAll()).thenReturn(List.of(h));
 
-        mockMvc.perform(get("/api/helprequests/all"))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$", hasSize(1)))
-            .andExpect(jsonPath("$[0].requesterEmail", is("a@b.com")))
-            .andExpect(jsonPath("$[0].teamId", is("T1")));
+    mockMvc
+        .perform(get("/api/helprequests/all"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$", hasSize(1)))
+        .andExpect(jsonPath("$[0].requesterEmail", is("a@b.com")))
+        .andExpect(jsonPath("$[0].teamId", is("T1")));
 
-        // Kills VoidMethodCall mutants
-        verify(helpRequestRepository).findAll();
-    }
-
-            
+    // Kills VoidMethodCall mutants
+    verify(helpRequestRepository).findAll();
+  }
 }
